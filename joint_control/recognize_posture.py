bc82afafb4f8b6537d12ec21ed2d15c6e33e1e56
Tim Seeger <tim.seeger@campus.tu-berlin.de>
2023-11-25 16:31:58 +0100

    fixed recognize posture

--- joint_control/recognize_posture.py
+++ joint_control/recognize_posture.py
@@ -24,23 +24,21 @@ class PostureRecognitionAgent(AngleInterpolationAgent):
                  sync_mode=True):
         super(PostureRecognitionAgent, self).__init__(simspark_ip, simspark_port, teamname, player_id, sync_mode)
         self.posture = 'unknown'
-        self.posture_classifier = None  # LOAD YOUR CLASSIFIER
+        self.posture_classifier = pickle.load(open('robot_pose.pkl', 'rb'))  # LOAD YOUR CLASSIFIER
 
     def think(self, perception):
         self.posture = self.recognize_posture(perception)
         return super(PostureRecognitionAgent, self).think(perception)
 
     def recognize_posture(self, perception):
-        #self.posture_classifier = pickle.load(open('robot_pose.pkl', 'rb'))
         posture = 'unknown'
         features = np.array([self.perception.joint['LHipYawPitch'], self.perception.joint['LHipRoll'], self.perception.joint['LHipPitch'],
                              self.perception.joint['LKneePitch'], self.perception.joint['RHipYawPitch'], self.perception.joint['RHipRoll'],
                              self.perception.joint['RHipPitch'], self.perception.joint['RKneePitch'], self.perception.imu[0], self.perception.imu[1]])
         features = features.reshape(1, -1)
-        #prediction = self.posture_classifier.predict(features)
-        #print(prediction)
-        posture = features
-        return posture
+        postureString = ['Left', 'HeadBack', 'Frog', 'Stand', 'Right', 'Sit', 'Knee', 'StandInit', 'Belly', 'Crouch', 'Back']
+        prediction = self.posture_classifier.predict(features)[0]
+        return postureString[prediction]
 
 if __name__ == '__main__':
     agent = PostureRecognitionAgent()
